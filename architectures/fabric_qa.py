83b51accf557a1b509e753fde7cf440904c41391
raulcf <raulcf@csail.mit.edu>
2017-05-26 10:25:38 -0400

    tweaks to fabric qa

--- architectures/fabric_qa.py
+++ architectures/fabric_qa.py
@@ -5,6 +5,10 @@ from keras.models import Model
 from keras.optimizers import SGD
 from keras.models import load_model
 
+from preprocessing.utils_pre import binary_encode as CODE
+from preprocessing.utils_pre import binary_decode as DECODE
+from postprocessing.utils_post import normalize_to_01_range
+
 
 def declare_model(input_dim):
     input_r = Input(shape=(input_dim,), name="input_r")
@@ -12,15 +16,18 @@ def declare_model(input_dim):
 
     r_merge_l = keras.layers.concatenate([input_r, input_l], name="r_merge_l")
 
-    inner_1 = Dense(128, activation='relu', name="inner_1")(r_merge_l)
-    #dropout_1 = Dropout(0.5, name="dropout1")(inner_1)
+    inner_1 = Dense(512, activation='relu', name="inner_1")(r_merge_l)
+    # dropout_1 = Dropout(0.5, name="dropout1")(inner_1)
 
-    inner_2 = Dense(128, activation='relu', name="inner_2")(inner_1)
-    #dropout_2 = Dropout(0.5, name="dropout2")(inner_2)
+    inner_2 = Dense(512, activation='relu', name="inner_2")(inner_1)
+    # dropout_2 = Dropout(0.5, name="dropout2")(inner_2)
+
+    inner_3 = Dense(512, activation='relu', name="inner_3")(inner_2)
+    # dropout_3 = Dropout(0.5, name="dropout3")(inner_3)
 
     #inner_3 = Dense(64, activation='relu', name="inner_3")(inner_2)
 
-    output = Dense(input_dim, activation='softmax', name="out")(inner_2)
+    output = Dense(input_dim, activation='softmax', name="out")(inner_3)
 
     model = Model(inputs=[input_r, input_l], outputs=output)
 
@@ -68,21 +75,9 @@ def load_model_from_path(path):
     return model
 
 
-if __name__ == "__main__":
-    print("fabric qa")
-
-    from preprocessing.utils_pre import binary_encode as CODE
-    from preprocessing.utils_pre import binary_decode as DECODE
-    import numpy as np
-    from postprocessing.utils_post import normalize_to_01_range
-
-    # vector = np.asarray([0.1, 0.00002, 0.005, 0.0083])
-    # nor = normalize_to_01_range(vector)
-    # print(nor)
-    # exit()
+def test_learn_to_sum():
 
     import random
-
     "learning to sum"
     training_data = []
     training_data_idx = []
@@ -90,7 +85,7 @@ if __name__ == "__main__":
         rnd = random.randint(0, 999)
         training_data_idx.append(rnd)
         x1 = [rnd]
-        x2 = [rnd+3]
+        x2 = [rnd + 3]
         y = [2 * rnd + 3]  # addition
         training_data.append((x1, x2, y))
         print(str(x1) + " + " + str(x2) + " = " + str(y))
@@ -145,14 +140,14 @@ if __name__ == "__main__":
         x2 = np.asarray([x2])
         total_ones_input = len(np.where(x1 == 0)[0]) + len(np.where(x2 == 1)[0])
         output = model.predict([x1, x2])
-        #avg = np.average(output)
-        #std = np.std(output)
-        #threshold = avg + std
+        # avg = np.average(output)
+        # std = np.std(output)
+        # threshold = avg + std
         threshold = 0.4
         normalized_output = normalize_to_01_range(output)
-        #threshold = np.percentile(output, 90)
+        # threshold = np.percentile(output, 90)
         ones = np.where(normalized_output > threshold)[1]
-        #ones = output[0].argsort()[-total_ones_input:][::1]
+        # ones = output[0].argsort()[-total_ones_input:][::1]
         bin_code = [0] * 32
         for one in ones:
             bin_code[one] = 1
@@ -163,7 +158,7 @@ if __name__ == "__main__":
         else:
             print(normalized_output)
         print(str(gt) + " -> " + str(result))
-    print("Hit ratio-training: " + str(float(hits/len(bin_training_data))))
+    print("Hit ratio-training: " + str(float(hits / len(bin_training_data))))
 
     hits = 0
     test_samples = 0
@@ -191,3 +186,80 @@ if __name__ == "__main__":
                 print(normalized_output)
             print(str(gt) + " -> " + str(result))
     print("Hit ratio-test: " + str(float(hits / test_samples)))
+
+
+def test_random_mapping():
+    import random
+    "learning to sum"
+    training_data = []
+    for i in range(1000):
+        x1 = [random.randint(0, 50)]
+        x2 = [random.randint(0, 50)]
+        y = [random.randint(0, 50)]
+        training_data.append((x1, x2, y))
+        print(str(x1) + " + " + str(x2) + " = " + str(y))
+
+    bin_training_data = []
+    for x1, x2, y in training_data:
+        x1_bin = CODE(x1)
+        x2_bin = CODE(x2)
+        y_bin = CODE(y)
+        bin_training_data.append((x1_bin, x2_bin, y_bin))
+        print(str(x1_bin) + " + " + str(x2_bin) + " = " + str(y_bin))
+
+    model = declare_model(32)
+    model = compile_model(model)
+
+    def generator_of_data(training_data, batch_size):
+        while 1:
+            batch_x1 = []
+            batch_x2 = []
+            batch_y = []
+            for i in range(len(training_data)):
+                if i == len(training_data):
+                    continue
+                x1, x2, y = training_data[i]
+                batch_x1.append(x1)
+                batch_x2.append(x2)
+                batch_y.append(y)
+                if len(batch_x1) == batch_size:
+                    yield [np.asarray(batch_x1), np.asarray(batch_x2)], np.asarray(batch_y)
+                    batch_x1.clear()
+                    batch_x2.clear()
+                    batch_y.clear()
+
+    import time
+    stime = time.time()
+    model = train_model_incremental(model, generator_of_data(bin_training_data, 2),
+                                            steps_per_epoch=500,
+                                            epochs=500)
+    etime = time.time()
+
+    hits = 0
+    for x1, x2, y in bin_training_data:
+        x1 = np.asarray([x1])
+        x2 = np.asarray([x2])
+        output = model.predict([x1, x2])
+        threshold = 0.4
+        normalized_output = normalize_to_01_range(output)
+        ones = np.where(normalized_output > threshold)[1]
+        bin_code = [0] * 32
+        for one in ones:
+            bin_code[one] = 1
+        gt = DECODE(y)
+        result = DECODE(bin_code)
+        if gt == result:
+            hits += 1
+        else:
+            print(normalized_output)
+        print(str(gt) + " -> " + str(result))
+    print("Hit ratio-training: " + str(float(hits / len(bin_training_data))))
+    print("Total training time: " + str(etime-stime))
+
+
+if __name__ == "__main__":
+    print("fabric qa")
+
+    test_random_mapping()
+
+
