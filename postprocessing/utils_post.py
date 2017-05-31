646c4972a89ea385649d4c0d49c5ba063ee45c38
raulcf <raulcf@csail.mit.edu>
2017-05-31 18:42:32 -0400

    bugfixes to normalization routines

--- postprocessing/utils_post.py
+++ postprocessing/utils_post.py
@@ -11,7 +11,7 @@ def normalize_to_01_range(vector):
     return np.asarray(normalized_vector)
 
 
-def normalize_per_dimension(vector, max_vector=None, min_vector=None):
+def normalize_to_unitrange_per_dimension(vector, max_vector=None, min_vector=None):
     num = np.asarray(vector) - np.asarray(min_vector)
     den = np.asarray(max_vector) - np.asarray(min_vector)
     norm = [0] * len(vector)
@@ -19,5 +19,16 @@ def normalize_per_dimension(vector, max_vector=None, min_vector=None):
         if den[idx] == 0:
             norm[idx] = 0
         else:
-            den[idx] = num[idx] / den[idx]
+            norm[idx] = num[idx] / den[idx]
+    return np.asarray(norm)
+
+
+def normalize_per_dimension(vector, mean_vector=None, std_vector=None):
+    num = np.asarray(vector) - np.asarray(mean_vector)
+    norm = [0] * len(vector)
+    for idx in range(len(vector)):
+        if std_vector[idx] == 0:
+            norm[idx] = 0
+        else:
+            norm[idx] = num[idx] / std_vector[idx]
     return np.asarray(norm)
