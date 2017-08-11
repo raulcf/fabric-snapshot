fb90e6a3a43c3e1922d78f6454b0800444bfa512
raulcf <raulcf@csail.mit.edu>
2017-08-11 19:19:00 +0000

    tweaks on use

--- evaluator_for_fabric.py
+++ evaluator_for_fabric.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
          path_csv,
          ptd_suffix + "nhcol_2seq_index/" + config.TF_DICTIONARY + ".pkl",
          ptd_suffix + "nhcol_2seq_index/bae/",
-         [el for el in range(251) if el % 5 == 0],
+         [el for el in range(250) if el % 5 == 0],
          "index",
          "accuracy",
          True),
@@ -20,7 +20,7 @@ if __name__ == "__main__":
          path_csv,
          ptd_suffix + "nhcol_2cyc_index/" + config.TF_DICTIONARY + ".pkl",
          ptd_suffix + "nhcol_2cyc_index/bae/",
-         [el for el in range(251) if el % 5 == 0],
+         [el for el in range(250) if el % 5 == 0],
          "index",
          "accuracy",
          True),
@@ -28,7 +28,7 @@ if __name__ == "__main__":
          path_csv,
          ptd_suffix + "nhrow_2seq_index/" + config.TF_DICTIONARY + ".pkl",
          ptd_suffix + "nhrow_2seq_index/bae/",
-         [el for el in range(251) if el % 5 == 0],
+         [el for el in range(250) if el % 5 == 0],
          "index",
          "accuracy",
          True),
@@ -36,7 +36,7 @@ if __name__ == "__main__":
          path_csv,
          ptd_suffix + "nhrow_2cyc_index/" + config.TF_DICTIONARY + ".pkl",
          ptd_suffix + "nhrow_2cyc_index/bae/",
-         [el for el in range(251) if el % 5 == 0],
+         [el for el in range(250) if el % 5 == 0],
          "index",
          "accuracy",
          True),
@@ -44,7 +44,7 @@ if __name__ == "__main__":
          path_csv,
          ptd_suffix + "nhrel_index/" + config.TF_DICTIONARY + ".pkl",
          ptd_suffix + "nhrel_index/bae/",
-         [el for el in range(251) if el % 5 == 0],
+         [el for el in range(250) if el % 5 == 0],
          "index",
          "accuracy",
          True)
@@ -52,17 +52,19 @@ if __name__ == "__main__":
 
     for el in args:
         print("# " + str(el[0]))
-        print("path_to_data: " + str(el[0]))
-        print("path_to_csv: " + str(el[1]))
-        print("path_to_vocab: " + str(el[2]))
-        print("path_to_bae_model: " + str(el[3]))
-        print("bae_model_epoch: " + str(el[4]))
-        print("encoding_mode: " + str(el[5]))
-        print("eval_task: " + str(el[6]))
-        print("experiment_output: " + str(el[7]))
         measurements = []
         for bae_epoch_number in el[4]:
             print("Eval epoch: " + str(bae_epoch_number))
+
+            print("path_to_data: " + str(el[0]))
+            print("path_to_csv: " + str(el[1]))
+            print("path_to_vocab: " + str(el[2]))
+            print("path_to_bae_model: " + str(el[3]))
+            print("bae_model_epoch: " + str(el[4]))
+            print("encoding_mode: " + str(el[5]))
+            print("eval_task: " + str(el[6]))
+            print("experiment_output: " + str(el[7]))
+
             exp_data = evaluate_fabric.main(path_to_data=el[0],
                                  path_to_csv=el[1],
                                  path_to_vocab=el[2],
@@ -71,9 +73,9 @@ if __name__ == "__main__":
                                  encoding_mode=el[5],
                                  eval_task=el[6],
                                  experiment_output=el[7])
-            for el in exp_data:
-                measurements.append(str(bae_epoch_number) + "," + el)
-        with open(str(el[3]) + "exp_data.csv", "w") as f:
+            for datum in exp_data:
+                measurements.append(str(bae_epoch_number) + "," + datum)
+        with open(str(el[3]) + "/exp_data.csv", "w") as f:
             f.write("# " + str(el[0]) + '\n')
-            for el in measurements:
-                f.write(el + '\n')
+            for datum in measurements:
+                f.write(datum + '\n')
