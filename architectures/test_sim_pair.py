627d13d69435c320a657caedc740b7d0c23f6759
raulcf <raulcf@csail.mit.edu>
2017-09-05 15:24:45 -0400

    fix on vectorizing complex data

--- architectures/test_sim_pair.py
+++ architectures/test_sim_pair.py
@@ -27,13 +27,11 @@ def main():
     o_path = "/data/eval/fbpair/"
     all_data, true_pairs = process_fb.extract_data_pairs()
 
-    exit()
-
     random_permutation = np.random.permutation(len(all_data))
     all_data = np.asarray(all_data)
     all_data = all_data[random_permutation]
-    with open(o_path + "true_pairs.pkl", "wb") as f:
-        pickle.dump(true_pairs, f)
+    # with open(o_path + "true_pairs.pkl", "wb") as f:
+    #     pickle.dump(true_pairs, f)
         # all_data = all_data[:2000]  # test
         # total = 0
         # for s, p, label in all_data:
