4afdc5ddc9a7ceb8e18fc47ea5b6d463d0ae00a2
raulcf <raulcf@csail.mit.edu>
2017-09-07 14:01:28 -0400

    sim pair for wiki data

--- architectures/test_sim_pair.py
+++ architectures/test_sim_pair.py
@@ -15,10 +15,80 @@ import pandas as pd
 from preprocessing.text_processor import IndexVectorizer, FlatIndexVectorizer
 from preprocessing import text_processor as tp
 
+wiki = True
 
 def main():
     o_path = "/data/eval/fbpair/"
     all_data, true_pairs = process_fb.extract_data_pairs()
+
+    if wiki:
+        o_path = "/data/eval/wikipair/"
+        # structured_path = "/Users/ra-mit/data/fabric/dbpedia/triples_structured/all.csv"
+        structured_path = "/data/smalldatasets/wiki/all.csv"
+        # unstructured_path = "/Users/ra-mit/data/fabric/dbpedia/triples_unstructured/"
+        unstructured_path = "/data/smalldatasets/wiki/triples_unstructured/"
+        spos = []
+        df = pd.read_csv(structured_path, encoding='latin1')
+        ss = list(df.iloc[:, 0])
+        ps = df.iloc[:, 1]
+        os = df.iloc[:, 2]
+        for s, p, o in zip(ss, ps, os):
+            spos.append((s, p, o))
+        print("Total structured spos: " + str(len(spos)))
+
+        from utils import prepare_sqa_data
+        uns_spos, loc_dic = prepare_sqa_data.get_spo_from_uns(path=unstructured_path)
+
+        print("Total unstructured spos: " + str(len(uns_spos)))
+
+        spos += uns_spos
+
+        all_data = []
+        all_right = []
+        all_pred = []
+        all_left = []
+        for s, p, o in spos:
+            all_left.append(s)
+            all_pred.append(p)
+            all_right.append(o)
+
+            all_data.append((s + " " + p, o, 0))
+            all_data.append((s, p + " " + o, 0))
+
+        print("True pairs: " + str(len(all_data)))
+
+        pos = set()
+        for e1, e2, label in all_data:
+            pos.add(e1 + e2)
+
+        # negative pairs
+        random_permutation = np.random.permutation(len(all_left))
+        all_left = np.asarray(all_left)
+        all_left = all_left[random_permutation]
+        random_permutation = np.random.permutation(len(all_right))
+        all_right = np.asarray(all_right)
+        all_right = all_right[random_permutation]
+
+        false_pairs = []
+        seen = set()
+        for s, p, o in zip(all_left, all_pred, all_right):
+            if s + " " + p + o in pos or s + p + " " + o in pos:
+                continue
+
+            if s + " " + p + o not in seen:
+                false_pairs.append((s + " " + p, o, 1))
+
+            if s + p + " " + o not in seen:
+                false_pairs.append((s, p + " " + o, 1))
+
+            seen.add((s + " " + p + o))
+            seen.add((s + p + " " + o))
+
+        print("False pairs: " + str(len(false_pairs)))
+
+        all_data += false_pairs
+
+        print("All pairs: " + str(len(all_data)))
 
     random_permutation = np.random.permutation(len(all_data))
     all_data = np.asarray(all_data)
@@ -37,6 +107,9 @@ def main():
 
     sparsity_code_size = 8
     idx_vectorizer = FlatIndexVectorizer(vocab_index=vocab, sparsity_code_size=sparsity_code_size)
+    if wiki:
+        sparsity_code_size = 48
+        idx_vectorizer = IndexVectorizer(vocab_index=vocab, sparsity_code_size=sparsity_code_size, tokenizer_sep=" ")
     vectorizer = tp.CustomVectorizer(idx_vectorizer)
 
     st = time.time()
