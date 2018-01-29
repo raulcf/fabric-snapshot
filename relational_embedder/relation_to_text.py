c3bedb32f4b78db2e83b1e9e2eb201ed1d9ea4d0
raulcf <raulcf@csail.mit.edu>
2018-01-29 10:34:55 -0500

    supporting multiple paths

--- relational_embedder/relation_to_text.py
+++ relational_embedder/relation_to_text.py
@@ -3,36 +3,39 @@ from data_prep import  data_prep_utils as dpu
 import os
 
 
-def serialize_row_and_column(path, output_file):
-    df = pd.read_csv(path, encoding='latin1')
-    columns = df.columns
+def serialize_row_and_column(paths, output_file):
+    for path in paths:
+        df = pd.read_csv(path, encoding='latin1')
+        columns = df.columns
 
-    try:
-        os.remove(output_file)
-    except FileNotFoundError:
-        print("Creating new file for writing data")
+        try:
+            os.remove(output_file)
+        except FileNotFoundError:
+            print("Creating new file for writing data")
 
-    with open(output_file, 'a') as f:
-        # Rows
-        for index, el in df.iterrows():
+        with open(output_file, 'a') as f:
+            # Rows
+            for index, el in df.iterrows():
+                for c in columns:
+                    cell_value = el[c]
+                    # clean cell_value
+                    cell_value = dpu.lowercase_removepunct(cell_value)
+                    cell_value = cell_value.replace(' ', '_')
+                    f.write(" " + cell_value)
+            # Columns
             for c in columns:
-                cell_value = el[c]
-                # clean cell_value
-                cell_value = dpu.lowercase_removepunct(cell_value)
-                cell_value = cell_value.replace(' ', '_')
-                f.write(" " + cell_value)
-        # Columns
-        for c in columns:
-            data_values = df[c]
-            for el in data_values:
-                el = dpu.lowercase_removepunct(el)
-                el = el.replace(' ', '_')
-                f.write(" " + el)
+                data_values = df[c]
+                for el in data_values:
+                    el = dpu.lowercase_removepunct(el)
+                    el = el.replace(' ', '_')
+                    f.write(" " + el)
 
 
 if __name__ == "__main__":
     print("Textify relation")
 
     path = "/Users/ra-mit/data/mitdwhdata/Se_person.csv"
+    path2 = "/Users/ra-mit/data/mitdwhdata/Drupal_employee_directory.csv"
+    paths = [path, path2]
 
-    serialize_row_and_column(path, "test.txt")
+    serialize_row_and_column(paths, "test.txt")
