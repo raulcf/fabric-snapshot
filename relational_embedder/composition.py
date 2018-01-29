9b3871b1069b6223de188d303b803ed75281b36c
raulcf <raulcf@csail.mit.edu>
2018-01-29 21:27:02 +0000

    tweaks to composition; added test notebook

--- relational_embedder/composition.py
+++ relational_embedder/composition.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import itertools
+from scipy.spatial.distance import cosine
 
 from data_prep import data_prep_utils as dpu
 import word2vec as w2v
@@ -10,21 +11,26 @@ def column_avg_composition(path, we_model):
     column_we = dict()
     df = pd.read_csv(path, encoding='latin1')
     columns = df.columns
-    col_wes = []
+    missing_words = 0
     for c in columns:
+        col_wes = []
         value = df[c]
         for el in value:
             el = dpu.encode_cell(el)
-            vector = we_model.get_vector(el)
+            try:
+                vector = we_model.get_vector(el)
+            except KeyError:
+                missing_words += 1
+                continue
             col_wes.append(vector)
         col_wes = np.asarray(col_wes)
-        col_we = np.average(col_wes)
+        col_we = np.mean(col_wes, axis=0)
         column_we[c] = col_we
-    return column_we
+    return column_we, missing_words
 
 
 def relation_column_composition(column_we):
-    relation_we = np.average(np.asarray(list(column_we.values())))
+    relation_we = np.mean(np.asarray(list(column_we.values())), axis=0)
     return relation_we
 
 
@@ -45,7 +51,7 @@ if __name__ == "__main__":
         we_a = col_wes[a]
         we_b = col_wes[b]
 
-        cos = np.dot(we_a, we_b)
+        cos = cosine(we_a, we_b)
         print(str(a) + " -sim- " + str(b) + " is: " + str(cos))
 
 
