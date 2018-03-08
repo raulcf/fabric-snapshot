82301a17574eb9802f9ce3f97ffa5a4b23d50d80
raulcf@csail.mit.edu <raulcf@csail.mit.edu>
2018-03-08 16:21:33 -0500

    Sketching ocmposition; textification strategies added: row and col

--- relational_embedder/relation_to_text.py
+++ relational_embedder/relation_to_text.py
@@ -5,6 +5,25 @@ from os import listdir
 from os.path import isfile, join
 
 
+def _read_rows_from_dataframe(df, columns):
+    for index, el in df.iterrows():
+        for c in columns:
+            cell_value = el[c]
+            # clean cell_value
+            cell_value = dpu.encode_cell(cell_value)
+            if cell_value == 'nan':  # probably more efficient to avoid nan upstream
+                continue
+            yield cell_value
+
+
+def _read_columns_from_dataframe(df, columns):
+    for c in columns:
+        data_values = df[c]
+        for el in data_values:
+            el = dpu.encode_cell(el)
+            yield el
+
+
 def serialize_row_and_column(paths, output_file, debug=False):
     try:
         os.remove(output_file)
@@ -21,18 +40,51 @@ def serialize_row_and_column(paths, output_file, debug=False):
         columns = df.columns
         with open(output_file, 'a') as f:
             # Rows
-            for index, el in df.iterrows():
-                for c in columns:
-                    cell_value = el[c]
-                    # clean cell_value
-                    cell_value = dpu.encode_cell(cell_value)
-                    f.write(" " + cell_value)
+            for cell_value in _read_rows_from_dataframe(df, columns):
+                f.write(" " + cell_value)
             # Columns
-            for c in columns:
-                data_values = df[c]
-                for el in data_values:
-                    el = dpu.encode_cell(el)
-                    f.write(" " + el)
+            for cell_value in _read_columns_from_dataframe(df, columns):
+                f.write(" " + cell_value)
+
+
+def serialize_row(paths, output_file, debug=False):
+    try:
+        os.remove(output_file)
+    except FileNotFoundError:
+        print("Creating new file for writing data")
+
+    total = len(paths)
+    current = 0
+    for path in paths:
+        if debug:
+            print(str(current) + "/" + str(total))
+            current += 1
+        df = pd.read_csv(path, encoding='latin1')
+        columns = df.columns
+        with open(output_file, 'a') as f:
+            # Rows
+            for cell_value in _read_rows_from_dataframe(df, columns):
+                f.write(" " + cell_value)
+
+
+def serialize_column(paths, output_file, debug=False):
+    try:
+        os.remove(output_file)
+    except FileNotFoundError:
+        print("Creating new file for writing data")
+
+    total = len(paths)
+    current = 0
+    for path in paths:
+        if debug:
+            print(str(current) + "/" + str(total))
+            current += 1
+        df = pd.read_csv(path, encoding='latin1')
+        columns = df.columns
+        with open(output_file, 'a') as f:
+            # Columns
+            for cell_value in _read_columns_from_dataframe(df, columns):
+                f.write(" " + cell_value)
 
 
 def all_files_in_path(path):
