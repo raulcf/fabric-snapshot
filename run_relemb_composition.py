8daf72bd92cd0219620fa5d85d27fb1859658d8c
raulcf@csail.mit.edu <raulcf@csail.mit.edu>
2018-04-05 11:31:14 -0400

    Caller for composition

---
+++ run_relemb_composition.py
@@ -0,0 +1,24 @@
+import argparse
+import pickle
+
+import word2vec as w2v
+from relational_embedder import composition
+
+
+if __name__ == "__main__":
+    print("Invoke composition of relational embedding")
+
+    # Argument parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--we_model', help='path to we model')
+    parser.add_argument('--method', default='avg', help='composition method')
+    parser.add_argument('--dataset', help='path to csv files')
+    parser.add_argument('--output', default='textified.txt', help='place to output relational embedding')
+
+    args = parser.parse_args()
+
+    we_model = w2v.load(args.we_model)
+    relational_embedding = composition.compose_dataset(args.dataset, we_model)
+    with open(args.output, 'wb') as f:
+        pickle.dump(relational_embedding, f)
+    print("Relational Embedding serialized to: " + str(args.output))
