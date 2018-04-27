4464ffcd79fb083d8dc0c60471a210819da4c378
raulcf@csail.mit.edu <raulcf@csail.mit.edu>
2018-04-27 16:30:36 -0400

    chunker bulk indexing for second baseline; gt commited

--- call_drqa_indexer.py
+++ call_drqa_indexer.py
@@ -1,7 +1,10 @@
 from qa_engine.dataset_indexers import drqa_data_indexer
+from qa_engine.dataset_indexers.drqa_data_indexer import IndexMode
 
-#drqa_data_indexer.main("128.52.171.0", 9200, "/data/datasets/wikipedia/data_drqa.csv")
-drqa_data_indexer.main("localhost", 9200, "/data/datasets/wikipedia/data_drqa.csv")
+imode = IndexMode.CHUNK
+
+# drqa_data_indexer.main("128.52.171.0", 9200, "/data/datasets/wikipedia/data_drqa.csv", mode=imode)
+drqa_data_indexer.main("localhost", 9200, "/data/datasets/wikipedia/data_drqa.csv", mode=imode)
 
 print("DONE!")
 
