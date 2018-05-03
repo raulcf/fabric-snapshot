7c18c107641a021960dbe5061f7d930f2ad556e4
raulcf@csail.mit.edu <raulcf@csail.mit.edu>
2018-05-03 12:54:22 -0400

    Updated composition runner

--- run_relemb_composition.py
+++ run_relemb_composition.py
@@ -11,14 +11,16 @@ if __name__ == "__main__":
 
     # Argument parsing
     parser = argparse.ArgumentParser()
-    parser.add_argument('--we_model', help='path to we model')
+    parser.add_argument('--row_we_model', help='path to row we model')
+    parser.add_argument('--col_we_model', help='path to col we model')
     parser.add_argument('--method', default='avg', help='composition method')
     parser.add_argument('--dataset', help='path to csv files')
     parser.add_argument('--output', default='textified.pkl', help='place to output relational embedding')
 
     args = parser.parse_args()
 
-    we_model = w2v.load(args.we_model)
+    row_we_model = w2v.load(args.row_we_model)
+    col_we_model = w2v.load(args.col_we_model)
 
     method = None
     if args.method == "avg":
@@ -26,7 +28,10 @@ if __name__ == "__main__":
     elif args.method == "avg_unique":
         method = CompositionStrategy.AVG_UNIQUE
 
-    relational_embedding = composition.compose_dataset(args.dataset, we_model, strategy=method)
-    with open(args.output, 'wb') as f:
-        pickle.dump(relational_embedding, f)
+    row_relational_embedding, col_relational_embedding = \
+        composition.compose_dataset(args.dataset, row_we_model, col_we_model, strategy=method)
+    with open(args.output + "/row.pkl", 'wb') as f:
+        pickle.dump(row_relational_embedding, f)
+    with open(args.output + "/col.pkl", 'wb') as f:
+        pickle.dump(col_relational_embedding, f)
     print("Relational Embedding serialized to: " + str(args.output))
