7b3df7a5857d029dcc8a245871eabfdf4c6660c5
raulcf <raulcf@csail.mit.edu>
2018-10-30 18:35:47 +0000

    config update

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 # QA Engine
-path_to_bidaf_model = "/Users/ra-mit/Downloads/bidaf-model-2017.09.15-charpad.tar.gz"
+#path_to_bidaf_model = "/Users/ra-mit/Downloads/bidaf-model-2017.09.15-charpad.tar.gz"
+path_to_bidaf_model = "/home/ubuntu/fabric/bidaf-model-2017.09.15-charpad.tar.gz"
